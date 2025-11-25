d7d84f9b0e2a9a23ffd32b34e3dc6da123789f7c
mustapha <mustaphaamoutaki@gmail.com>
2025-11-25 11:14:56 +0100

    initial commit

---
+++ src/test/java/org/mustapha/smartShop/SmartShopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mustapha.smartShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
